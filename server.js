afe332386785bc62c49c4f50af0eea22ddf473d4
adam0ling <adomas.lingevicius@gmail.com>
2020-04-21 18:04:18 +0300

    works

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
