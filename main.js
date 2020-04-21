d1b2b105a7bbc83777ce6068c0b06d5a25be1ad3
adam0ling <adomas.lingevicius@gmail.com>
2020-04-21 17:10:25 +0300

    hopefully this works

--- main.js
+++ main.js
@@ -205,3 +205,15 @@ class Main {
 }
 
 window.addEventListener('load', () => new Main());
+
+var Backbone = require('backbone');
+var $ = Backbone.$ = require('jquery/dist/jquery')(window);
+
+var AppView = Backbone.View.extend({
+  render: function(){
+    $('main').append('<h1>Browserify is mathematical.</h1>');
+  }
+});
+
+var appView = new AppView();
+appView.render();
